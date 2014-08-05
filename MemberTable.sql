9c93afbc4a4cdece85fba2e6fe08e2ff0222f5e8
CoryKoehler <cjiek9181@gmail.com>
2014-08-05 13:58:36 -0400

    Added SQL Scripts
    Updated DB scripts and tables

---
+++ MemberTable.sql
@@ -0,0 +1,19 @@
+USE [AlumniDB]
+GO
+
+SELECT [Member_Id]
+      ,[FirstName]
+      ,[MiddleInitial]
+      ,[LastName]
+      ,[FaceBook]
+      ,[Google]
+      ,[Twitter]
+      ,[LinkedIn]
+      ,[GitHub]
+      ,[Email]
+      ,[PhoneNumber]
+      ,[EmployerName]
+      ,[UserPhoto]
+  FROM [dbo].[Member]
+GO
+
