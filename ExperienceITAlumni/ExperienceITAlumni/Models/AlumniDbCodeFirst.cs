56ebc4e3d9637e405a55b7a8a5f302b898a7d181
CoryKoehler <cjiek9181@gmail.com>
2014-08-11 11:42:14 -0400

    Fixed extra using statements

--- ExperienceITAlumni/ExperienceITAlumni/Models/AlumniDbCodeFirst.cs
+++ ExperienceITAlumni/ExperienceITAlumni/Models/AlumniDbCodeFirst.cs
@@ -4,8 +4,7 @@ using System.Data.Entity;
 using System.Linq;
 using System.ComponentModel.DataAnnotations;
 using System.Collections.Generic;
-using System.Data;using System.Collections.Generic;
-using System.Linq;
+using System.Data;
 using System.Web;
 
 namespace ExperienceITAlumni.Models
