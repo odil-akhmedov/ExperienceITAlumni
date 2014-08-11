aeb82dccfd79e435a28dde4003cc4616d24fffbf
CoryKoehler <cjiek9181@gmail.com>
2014-08-11 11:49:52 -0400

    Chat controller changes brought in from CK

--- ExperienceITAlumni/ExperienceITAlumni/Controllers/ChatController.cs
+++ ExperienceITAlumni/ExperienceITAlumni/Controllers/ChatController.cs
@@ -1,16 +1,34 @@
-﻿using System;
+﻿﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using ExperienceITAlumni.Models;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
 
 namespace ExperienceITAlumni.Controllers
 {
     public class ChatController : Controller
     {
+        //private ApplicationDbContext db = new ApplicationDbContext();
         // GET: Chat
         public ActionResult Index()
         {
+            //var user = await UserManager.FindByIdAsync(id);
+            //if (id == null)
+            //{
+            //    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            //}
+            //Members members = db.Members.Where(x => x.UserId == user.Id).Single();
+            //if (members == null)
+            //{
+            //    return HttpNotFound();
+            //}
             return View();
         }
 
